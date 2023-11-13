cb8c1e1c9c7c1dc4aa2d19437e7cfc8ec2a0a1e8
toumen <2942315390@qq.com>
2023-11-13 20:29:02 +0800

    11.13

---
+++ 代码/题/cpp/part.c
@@ -0,0 +1,26 @@
+/*
+#include<stdio.h>int main()
+{printf("Hellow World!\n"
+);  return 0;}*/
+/*
+#include<stdio.h>
+int main()
+{
+    int n, n2, n3;
+    n=5;
+    n2=n*n;
+    n3=n2*n2;
+    printf("n = %d\nn2 = %d\nn3 = %d\n",n,n2,n3);
+    return 0;
+}*/
+/*
+#include<stdio.h>
+int main()
+{
+    char a=7;
+    a=getchar();
+    putchar(a);
+    printf("\a");
+    return 0;
+}*/
+//第四章
