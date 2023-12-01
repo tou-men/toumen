e9be93bcf39d162ab57652d7f95275837fc3499b
toumen <2942315390@qq.com>
2023-12-01 16:28:38 +0800

    12.1

---
+++ 代码/plan/1/strbuf.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<string.h>
+struct strbuf
+{
+    int len;     //当前缓冲区（字符串）长度
+    int alloc;   //当前缓冲区（字符串）容量
+    char *buf;   //缓冲区（字符串）
+};
+void strbuf_init(struct strbuf *sb, size_t alloc);
+void strbuf_attach(struct strbuf *sb, void *str, size_t len, size_t alloc);
+void strbuf_release(struct strbuf *sb);
+void strbuf_swap(struct strbuf *a, struct strbuf *b);
+int main()
+{
+    struct strbuf sb;
+    strbuf_init(&sb, 10);
+    strbuf_attach(&sb, "xiyou", 5, 10);
+    assert(strcmp(sb.buf, "xiyou") == 0);
+    strbuf_addstr(&sb, "linux");
+    assert(strcmp(sb.buf, "xiyoulinux") == 0);
+    strbuf_release(&sb);
+}
+void strbuf_init(struct strbuf *sb, size_t alloc)
+{
+    sb=malloc(sizeof(int)*2+sizeof(char)*alloc);
+    sb->alloc=alloc;
+}
+void strbuf_attach(struct strbuf *sb, void *str, size_t len, size_t alloc)
+{
+    strcpy(sb->buf,str);
+    sb->len=len;
+    sb->alloc=alloc;
+}
+void strbuf_release(struct strbuf *sb)
+{
+    free(sb);
+}
+void strbuf_swap(struct strbuf *a, struct strbuf *b)
+{
+    int t=a->alloc;
+    a->alloc=b->alloc;
+    b->alloc=t;
+    t=a->len;
+    a->len=b->len;
+    b->len=t;
+    
+}
