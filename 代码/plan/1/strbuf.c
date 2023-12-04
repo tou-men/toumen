a94d7d94e7119aea44299e39847d4e0c245f13e4
toumen <2942315390@qq.com>
2023-12-04 19:59:02 +0800

    12.4

--- 代码/plan/1/strbuf.c
+++ 代码/plan/1/strbuf.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 #include<string.h>
+#include<stdlib.h>
+//#include"strbuf.h"
 struct strbuf
 {
     int len;     //当前缓冲区（字符串）长度
@@ -10,9 +12,11 @@ void strbuf_init(struct strbuf *sb, size_t alloc);//
 void strbuf_attach(struct strbuf *sb, void *str, size_t len, size_t alloc);//
 void strbuf_release(struct strbuf *sb);//
 void strbuf_swap(struct strbuf *a, struct strbuf *b);//
-char *strbuf_detach(struct strbuf *sb, size_t *sz);
+char *strbuf_detach(struct strbuf *sb, size_t *sz);//
 int strbuf_cmp(const struct strbuf *first, const struct strbuf *second);//
 void strbuf_reset(struct strbuf *sb);//
+void strbuf_grow(struct strbuf *sb, size_t extra);//
+void strbuf_add(struct strbuf *sb, const void *data, size_t len);//----
 int main()
 {
     struct strbuf sb;
@@ -25,7 +29,7 @@ int main()
 }
 void strbuf_init(struct strbuf *sb, size_t alloc)
 {
-    sb=malloc(sizeof(int)*2+sizeof(char)*alloc);
+    sb=(char *)malloc(sizeof(int)*2+sizeof(char)*alloc);
     sb->alloc=alloc;
 }
 void strbuf_attach(struct strbuf *sb, void *str, size_t len, size_t alloc)
@@ -59,4 +63,14 @@ int strbuf_cmp(const struct strbuf *first, const struct strbuf *second)
 void strbuf_reset(struct strbuf *sb)
 {
     for(int i=0;i<sb->len;i++) sb->buf[i]='\0';
+}
+void strbuf_grow(struct strbuf *sb, size_t extra)
+{
+    int n=sb->alloc-sb->len;
+    if(n<extra) sb->buf=(char *)realloc(sb->buf,(sb->len+extra)*sizeof(char));
+}
+void strbuf_add(struct strbuf *sb, const void *data, size_t len)
+{
+    strbuf_grow(sb,len+1);
+    for(int i=0;i<len;i++) sb->buf[len+i]=&data[i];
 }
