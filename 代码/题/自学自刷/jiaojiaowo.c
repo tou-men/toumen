448fee920a782d8d2becc954993e9c1cae4d2011
toumen <2942315390@qq.com>
2023-12-09 16:58:00 +0800

    12.9

--- 代码/题/自学自刷/jiaojiaowo.c
+++ 代码/题/自学自刷/jiaojiaowo.c
@@ -281,6 +281,20 @@ int main()
     printf("%d",max-min);
     return 0;
 }*/
+/*#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a=0;
+    while(n)
+    {
+        a=n%10+a*10;
+        n/=10;
+    }
+    printf("%d\n",a);
+    return 0;
+}*/
 
 
 
