81d3f9cd9548b562ac38da9f994d007853ce3cce
toumen <2942315390@qq.com>
2023-12-28 23:33:43 +0800

    12.28

---
+++ c/题/pta/12.12.c
@@ -0,0 +1,37 @@
+/*
+void Shift( char s[] )
+{
+    int len=strlen(s);
+    char ch;
+    for(int i=0;i<3;i++)
+    {
+        ch=s[0];
+        for(int j=1;j<len;j++)
+        {
+            s[j-1]=s[j];
+        }
+        s[len-1]=ch;
+    }
+}*/
+/*
+void splitfloat( float x, int *intpart, float *fracpart )
+{
+    *intpart=(int)x;
+    *fracpart=x-*intpart;
+}*/
+/*
+int search( int list[], int n, int x )
+{
+    int i,flag=1,N;
+    for(i=0;i<n;i++)
+    {
+        if(list[i]==x)
+        {
+            flag=-1;
+            N=i;
+            break;
+        }
+    }
+    if(flag==1) return -1;
+    if(flag==-1) return N;
+}*/
