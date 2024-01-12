b8edb11306cc93b305affe38c6e99a6f3f1ba24e
toumen <2942315390@qq.com>
2024-01-12 10:54:11 +0800

    1.12

---
+++ c/题/pta/pta.12.7.c
@@ -0,0 +1,12 @@
+/*
+int getindex( char *s )
+{
+    if(strncmp(s,"Sunday",strlen(s))==0) return 0;
+    else if(strncmp(s,"Monday",strlen(s))==0) return 1;
+    else if(strncmp(s,"Tuesday",strlen(s))==0) return 2;
+    else if(strncmp(s,"Wednesday",strlen(s))==0) return 3;
+    else if(strncmp(s,"Thursday",strlen(s))==0) return 4;
+    else if(strncmp(s,"Friday",strlen(s))==0) return 5;
+    else if(strncmp(s,"Saturday",strlen(s))==0) return 6;
+    else return -1;
+}*///-------------------------------------------------------------------------
