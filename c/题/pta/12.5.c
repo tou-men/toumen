e9f140a2633a7956b06bb0f34221eef426e6f864
toumen <2942315390@qq.com>
2024-01-19 10:52:08 +0800

    1.19

--- c/题/pta/12.5.c
+++ c/题/pta/12.5.c
@@ -4,3 +4,9 @@ void sum_diff( float op1, float op2, float *psum, float *pdiff )
     *psum=op1+op2;
     *pdiff=op1-op2;
 }*///-----------------------------------------------------------------
+/*
+void findmax( int *px, int *py, int *pmax )
+{
+    if(*px>*py) *pmax=*px;
+    else *pmax=*py;
+}*///------------------------------------------------------------------------
