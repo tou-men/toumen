9dd80351cd92a5f231411cc9fda630caa4a3e259
toumen <2942315390@qq.com>
2024-01-21 19:12:21 +0800

    1.21

--- c/题/pta/12.5.c
+++ c/题/pta/12.5.c
@@ -10,3 +10,13 @@ void findmax( int *px, int *py, int *pmax )
     if(*px>*py) *pmax=*px;
     else *pmax=*py;
 }*///------------------------------------------------------------------------
+/*
+int FindArrayMax( int a[], int n )
+{
+    int max=a[0];
+    for(int i=1;i<n;i++)
+    {
+        if(max<a[i]) max=a[i];
+    }
+    return max;
+}*///-------------------------------------------------------------------------
